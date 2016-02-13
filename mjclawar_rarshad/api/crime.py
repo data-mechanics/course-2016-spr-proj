0a3607af58cbf4463822a2b39aba33e6b5a4610e
Michael Clawar <clawar@stratodem.com>
2016-02-13 18:37:39 -0500

    Provenance as a class

--- mjclawar_rarshad/api/crime.py
+++ mjclawar_rarshad/api/crime.py
@@ -14,7 +14,7 @@ from mjclawar_rarshad import mcra_structures as mcras
 from mjclawar_rarshad.database_helpers import DatabaseHelper
 from mjclawar_rarshad.api.bdp_query import BDPQuery
 from mjclawar_rarshad.mcra_structures import APIQuery, MCRASSettings, MCRASProvenance
-from mjclawar_rarshad.setup_provenance import load_provenance_json, write_provenance_json
+from mjclawar_rarshad.setup_provenance import ProjectProvenance
 
 
 class CrimeSettings(MCRASSettings):
@@ -36,46 +36,47 @@ class CrimeSettings(MCRASSettings):
 
 
 class CrimeProvenance(MCRASProvenance):
-    def __init__(self, settings, query=''):
+    def __init__(self, settings, prov_doc, query=''):
         assert isinstance(settings, CrimeSettings)
+        assert isinstance(prov_doc, ProjectProvenance)
         assert isinstance(query, str)
         self.settings = settings
         self.query = query
+        self.prov_doc = prov_doc.prov_doc
 
     def update_provenance(self, start_time, end_time):
-        prov_doc = load_provenance_json()
-        this_script = prov_doc.agent(self.settings.agent, mcras.PROVENANCE_PYTHON_SCRIPT)
+        this_script = self.prov_doc.agent(self.settings.agent, mcras.PROVENANCE_PYTHON_SCRIPT)
 
-        resource = prov_doc.entity('%s:%s' % (self.settings.data_namespace.name,
+        resource = self.prov_doc.entity('%s:%s' % (self.settings.data_namespace.name,
                                               self.settings.base_url))
 
-        this_run = prov_doc.activity('%s:a%s' % (mcras.LOG_NAMESPACE.name, str(uuid.uuid4())), start_time, end_time,
+        this_run = self.prov_doc.activity('%s:a%s' % (mcras.LOG_NAMESPACE.name, str(uuid.uuid4())), start_time, end_time,
                                      {prov.model.PROV_TYPE: mcras.PROVENANCE_ONT_RETRIEVAL,
                                       mcras.PROV_ONT_QUERY: '?' + self.query})
 
-        prov_doc.wasAssociatedWith(this_run, this_script)
-        prov_doc.used(this_run, resource, start_time)
+        self.prov_doc.wasAssociatedWith(this_run, this_script)
+        self.prov_doc.used(this_run, resource, start_time)
 
-        data_doc = prov_doc.entity('%s:%s' % (mcras.DAT_NAMESPACE.name, self.settings.data_entity),
+        data_doc = self.prov_doc.entity('%s:%s' % (mcras.DAT_NAMESPACE.name, self.settings.data_entity),
                                    {prov.model.PROV_LABEL: 'Crimes Committed', prov.model.PROV_TYPE:
                                        mcras.PROV_ONT_DATASET})
 
-        prov_doc.wasAttributedTo(data_doc, this_script)
-        prov_doc.wasGeneratedBy(data_doc, this_run, end_time)
-        prov_doc.wasDerivedFrom(data_doc, resource, this_run, this_run, this_run)
-
-        write_provenance_json(prov_doc)
+        self.prov_doc.wasAttributedTo(data_doc, this_script)
+        self.prov_doc.wasGeneratedBy(data_doc, this_run, end_time)
+        self.prov_doc.wasDerivedFrom(data_doc, resource, this_run, this_run, this_run)
 
 
 class CrimeAPIQuery(APIQuery):
-    def __init__(self, settings, database_helper, bdp_api):
+    def __init__(self, settings, database_helper, bdp_api, project_provenance):
         assert isinstance(settings, CrimeSettings)
         assert isinstance(database_helper, DatabaseHelper)
         assert isinstance(bdp_api, BDPQuery)
+        assert isinstance(project_provenance, ProjectProvenance)
 
         self.settings = settings
         self.database_helper = database_helper
         self.bdp_api = bdp_api
+        self.project_provenance = project_provenance
 
     def download_update_database(self):
         start_time = datetime.datetime.now()
@@ -85,5 +86,5 @@ class CrimeAPIQuery(APIQuery):
 
         end_time = datetime.datetime.now()
 
-        crime_provenance = CrimeProvenance(self.settings, query=api_query)
+        crime_provenance = CrimeProvenance(self.settings, prov_doc=self.project_provenance, query=api_query)
         crime_provenance.update_provenance(start_time=start_time, end_time=end_time)
--- mjclawar_rarshad/setup_provenance.py
+++ mjclawar_rarshad/setup_provenance.py
@@ -1,31 +1,21 @@
 from prov.model import ProvDocument
 
 from mjclawar_rarshad import mcra_structures as mcras
-from mjclawar_rarshad import reference, database_helpers
+from mjclawar_rarshad import reference
+from mjclawar_rarshad.database_helpers import DatabaseHelper
 
 
-def load_provenance_json():
-    # TODO load from repo
-    provenance_document = ProvDocument.deserialize(reference.provenance_file)
-    return provenance_document
+class ProjectProvenance:
+    def __init__(self, database_helper):
+        assert isinstance(database_helper, DatabaseHelper)
 
+        self.database_helper = database_helper
+        self.prov_doc = ProvDocument()
+        self.prov_doc.add_namespace(mcras.BDP_NAMESPACE.name, mcras.BDP_NAMESPACE.link)
+        self.prov_doc.add_namespace(mcras.ALG_NAMESPACE.name, mcras.ALG_NAMESPACE.link)
+        self.prov_doc.add_namespace(mcras.DAT_NAMESPACE.name, mcras.DAT_NAMESPACE.link)
+        self.prov_doc.add_namespace(mcras.LOG_NAMESPACE.name, mcras.LOG_NAMESPACE.link)
+        self.prov_doc.add_namespace(mcras.ONT_NAMESPACE.name, mcras.ONT_NAMESPACE.link)
 
-def initialize_provenance():
-    prov_doc = ProvDocument()
-    prov_doc.add_namespace(mcras.BDP_NAMESPACE.name, mcras.BDP_NAMESPACE.link)
-    prov_doc.add_namespace(mcras.ALG_NAMESPACE.name, mcras.ALG_NAMESPACE.link)
-    prov_doc.add_namespace(mcras.DAT_NAMESPACE.name, mcras.DAT_NAMESPACE.link)
-    prov_doc.add_namespace(mcras.LOG_NAMESPACE.name, mcras.LOG_NAMESPACE.link)
-    prov_doc.add_namespace(mcras.ONT_NAMESPACE.name, mcras.ONT_NAMESPACE.link)
-
-    write_provenance_json(prov_doc)
-
-
-def write_provenance_json(prov_doc):
-    assert isinstance(prov_doc, ProvDocument)
-    prov_doc.serialize(reference.provenance_file)
-
-
-if __name__ == '__main__':
-    initialize_provenance()
-    print('Done!')
+    def write_provenance_json(self):
+        self.prov_doc.serialize(reference.provenance_file)
