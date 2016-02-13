cf2288ba1586c1e2e8f53147b491af19af45b219
raaidarshad <raaidarshad@gmail.com>
2016-02-13 16:17:58 -0500

    Updated setup_repo.

--- mjclawar_rarshad/api/setup_repo.py
+++ mjclawar_rarshad/api/setup_repo.py
@@ -5,17 +5,19 @@ Michael Clawar and Raaid Arshad.
 """
 import pymongo
 
-
-client = pymongo.MongoClient()
-repo = client.repo
-repo.authenticate('mjclawar_rarshad', 'mjclawar_rarshad')
+exec(open('../pymongo_dm.py').read())
 
 
-def connect_to_db():
-    pass
+def connect_repo():
+    client = pymongo.MongoClient()
+    repo = client.repo
+    repo.authenticate('mjclawar_rarshad', 'mjclawar_rarshad')
+    return repo
 
 
-def insert_permanent_db():
-    pass
-
-
+def insert_db(dbname, thedata):
+    assert isinstance(dbname, str)
+    repo = connect_repo()
+    repo.dropPermanent(dbname)
+    repo.createPermanent(dbname)
+    repo['mjclawar_rarshad.%s' % dbname].insert_many(thedata)
