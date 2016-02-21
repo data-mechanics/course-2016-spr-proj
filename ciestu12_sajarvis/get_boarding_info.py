1e84582e1bf05da041f3f1b862c76ca0298b63a5
Steve Jarvis <sajarvis@bu.edu>
2016-02-21 17:32:32 -0500

    Create provenance data for boarding info

--- ciestu12_sajarvis/get_boarding_info.py
+++ ciestu12_sajarvis/get_boarding_info.py
@@ -33,6 +33,28 @@ repo['{}.{}'.format(teamname, collection)].insert_many(r)
 
 endTime = datetime.datetime.now()
 
-# TODO provenance data and recording
+# Create provenance data and recording
+doc = prov.model.ProvDocument()
+doc.add_namespace('alg', 'http://datamechanics.io/algorithm/ciestu12_sajarvis/') # The scripts in <folder>/<filename> format.
+doc.add_namespace('dat', 'http://datamechanics.io/data/ciestu12_sajarvis/') # The data sets in <user>/<collection> format.
+doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
+doc.add_namespace('log', 'http://datamechanics.io/log#') # The event log.
+doc.add_namespace('bu', 'http://cs-people.bu.edu/sajarvis/datamech/')
+
+this_script = doc.agent('alg:get_boarding_info', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+resource = doc.entity('bu:green_line_boarding', {'prov:label':'Green Line Boarding Counts', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+# No additional query needed for this data
+this_run = doc.activity('log:a'+str(uuid.uuid4()), startTime, endTime, {prov.model.PROV_TYPE:'ont:Retrieval', 'ont:Query':''})
+doc.wasAssociatedWith(this_run, this_script)
+doc.used(this_run, resource, startTime)
+
+lost = doc.entity('dat:boardings', {prov.model.PROV_LABEL:'Green Line Stop Boardings', prov.model.PROV_TYPE:'ont:DataSet'})
+doc.wasAttributedTo(lost, this_script)
+doc.wasGeneratedBy(lost, this_run, endTime)
+doc.wasDerivedFrom(lost, resource, this_run, this_run, this_run)
+
+repo.record(doc.serialize()) # Record the provenance document.
+#print(json.dumps(json.loads(doc.serialize()), indent=4))
+print(doc.get_provn())
 
 repo.logout()
