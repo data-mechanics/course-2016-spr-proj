6ab25dc90fb79756309a1951ce048bf5b814b322
Steve Jarvis <sajarvis@bu.edu>
2016-02-21 17:08:27 -0500

    Updated and added some commends to ppl-sec script

--- ciestu12_sajarvis/make_people_seconds.py
+++ ciestu12_sajarvis/make_people_seconds.py
@@ -1,27 +1,23 @@
 """
-Query Google maps for walking distances between T stops.
+Use the stop boarding counts and nearest stop datasets to come up with a
+measure of utility provided by each stop, called 'people seconds'. This
+measure can be used to determine the value of each stop.
 
-Get walking distance and time from every Green Line stop to every other.
+Requires the collections:
+    'nearest_stops'
+    'boarding_counts'
 
-It's a combination of GPS coordinates and line information from T data and
-distances from Google Maps API.
-
-Relies on these collections already existing in the database:
-    t_branch_info
-    t_stop_locations
-
-It also takes quite a while, due to rate limiting at the free tier of Google's
-API.
+Lower scores indicate a higher value, considering the overall time saved for
+commuters.
 """
-import urllib.request
 import json
 import pymongo
 import prov.model
 import time
 import datetime
 import uuid
-import sys
 
+# Estimated time of the time cost (seconds) for stopping at a T stop.
 STOP_TIME = 60
 
 # Until a library is created, we just use the script directly.
@@ -32,6 +28,9 @@ teamname = 'ciestu12_sajarvis'
 client = pymongo.MongoClient()
 repo = client.repo
 repo.authenticate(teamname, teamname)
+out_coll = 'people_second_utility'
+repo.dropPermanent(out_coll)
+repo.createPermanent(out_coll)
 
 startTime = datetime.datetime.now()
 
@@ -42,20 +41,18 @@ def product(R, S):
 stop_pop = repo['{}.{}'.format(teamname, 'boarding_counts')].find({})
 nearest_stops = repo['{}.{}'.format(teamname, 'nearest_stops')].find({})
 
-nearest = [ (s['stop'], s['line'], s['nearest'],s['time_sec']) for s in nearest_stops]
-pop = [ (s['stop_id'], s['stop_boardings']) for s in stop_pop]
+# construct some tuples of the information we need
+nearest = [(s['stop'], s['line'], s['nearest'], s['time_sec']) for s in nearest_stops]
+pop = [(s['stop_id'], s['stop_boardings']) for s in stop_pop]
 
 dot = product(nearest, pop)
-matches = [ (f,g) for (f,g) in dot if f[0] == g[0] ]
-
-out_coll = 'people_second_utility'
-repo.dropPermanent(out_coll)
-repo.createPermanent(out_coll)
+matches = [(f,g) for (f,g) in dot if f[0] == g[0]]
 
 for line in ['GLB', 'GLC', 'GLD', 'GLE']:
     total_usage = sum([pop for ((s,l,n,w),(i,pop)) in matches if l == line])
     for stop,pop,sec in [(s,p,w) for ((s,l,n,w),(i,p)) in matches if l == line]:
         everyone_else = total_usage - pop
+        # the actual measure of utility. low scores are best.
         ppl_seconds = (everyone_else * STOP_TIME) - (pop * sec)
         # now insert the people second utility into our data set.
         elements = {'stop':stop, 'ppl-secs':ppl_seconds, 'line': line}
