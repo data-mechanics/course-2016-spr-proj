14ccc5a241dfc180248540ab2fd2735a46653074
Cristina Estupiñán <ciestupinan@users.noreply.github.com>
2016-02-22 20:20:11 -0500

    Added Provenance to get_green_line_branch_info.py

--- ciestu12_sajarvis/get_green_line_branch_info.py
+++ ciestu12_sajarvis/get_green_line_branch_info.py
@@ -34,6 +34,27 @@ repo['{}.{}'.format(teamname, collection)].insert_many(r)
 
 endTime = datetime.datetime.now()
 
-# TODO provenance data and recording
+# Create provenance data and recording
+doc = prov.model.ProvDocument()
+doc.add_namespace('alg', 'http://datamechanics.io/algorithm/ciestu12_sajarvis/') # The scripts in <folder>/<filename> format.
+doc.add_namespace('dat', 'http://datamechanics.io/data/ciestu12_sajarvis/') # The data sets in <user>/<collection> format.
+doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
+doc.add_namespace('log', 'http://datamechanics.io/log#') # The event log.
+doc.add_namespace('bu', 'http://cs-people.bu.edu/sajarvis/datamech/')
+
+this_script = doc.agent('alg:get_green_line_branch_info', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+resource = doc.entity('bu:green_line_branch_info', {'prov:label':'Green Line Branch Info', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+# No additional query needed for this data
+this_run = doc.activity('log:a'+str(uuid.uuid4()), startTime, endTime, {prov.model.PROV_TYPE:'ont:Retrieval', 'ont:Query':''})
+doc.wasAssociatedWith(this_run, this_script)
+doc.used(this_run, resource, startTime)
+
+branch_stops = doc.entity('dat:branch_stops', {prov.model.PROV_LABEL:'Green Line Branch Stops', prov.model.PROV_TYPE:'ont:DataSet'})
+doc.wasAttributedTo(boardings, this_script)
+doc.wasGeneratedBy(branch_stops, this_run, endTime)
+doc.wasDerivedFrom(branch_stops, resource, this_run, this_run, this_run)
+
+repo.record(doc.serialize()) # Record the provenance document.
+print(doc.get_provn())
 
 repo.logout()
