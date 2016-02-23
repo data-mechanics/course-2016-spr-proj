3143b2ffa8c507d0baa255b0c9d3c1d429d9b818
Joseph Muruguru <jmuru1@bu.edu>
2016-02-23 15:17:00 -0500

    successfully extracted zipcodes from ticket locations

--- geo.py
+++ geo.py
@@ -16,6 +16,15 @@ of possible. Specifically in regards to how
 we deal with addresses with no numbers given
 '''
 
+'''
+response scheme: 
+geometry
+place_id
+formatted_address
+address_components
+types
+'''
+
 ticketsUrl = 'https://data.cityofboston.gov/resource/cpdb-ie6e.json' + '?$$app_token=' + data["api_key"]
 # generate list of addresses
 (rawAddr, formatted) = apitest.request(ticketsUrl)
@@ -23,15 +32,17 @@ ticketsUrl = 'https://data.cityofboston.gov/resource/cpdb-ie6e.json' + '?$$app_t
 def generateAddr(json_obj):
 	return [elem['ticket_loc'] for elem in json_obj]
 
-#print(generateAddr(rawAddr))
-
 def locationQueryHelper(addr):
 	queryAddr = addr + ', Boston, Ma'
 	geocode_result = gmaps.geocode(queryAddr)
-	return str(geocode_result[0]['geometry']['location_type'])
+	return ((geocode_result[0]['address_components']), str(geocode_result[0]['geometry']['location_type']))
 
 def locationQuery(addr_list):
-	return [locationQueryHelper(elem) for elem in addr_list if locationQueryHelper(elem) != 'APPROXIMATE']
+	#extracts zip code from addresses that got tickets
+	return [locationQueryHelper(ADDR_TICKETS[0])[0][len(locationQueryHelper(ADDR_TICKETS[0])[0])-1]['short_name'] for elem in addr_list if locationQueryHelper(elem)[1] != 'APPROXIMATE']
+
+ADDR_TICKETS = generateAddr(rawAddr)
+print(locationQuery(ADDR_TICKETS[900:950]))
 
 
 
