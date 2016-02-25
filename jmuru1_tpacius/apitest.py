0c4eaaa55cc2dd5b60982ee15015083db39082ca
Joseph Muruguru <jmuru1@bu.edu>
2016-02-24 21:16:40 -0500

    Changed file structure

---
+++ jmuru1_tpacius/apitest.py
@@ -0,0 +1,25 @@
+import urllib.request
+import json
+import pymongo
+import prov.model
+import datetime
+import uuid
+
+with open('auth.json') as credentials:
+	data = json.load(credentials)
+
+zipcarUrl1 = 'https://data.cityofboston.gov/resource/78f5-5i4e.json' + '?$$app_token=' + data["api_key"] + "&$limit=1000"
+zipcarUrl2 = 'https://data.cityofboston.gov/resource/498g-jbmi.json' + '?$$app_token=' + data["api_key"] + "&$limit=1000"
+ticketsUrl = 'https://data.cityofboston.gov/resource/cpdb-ie6e.json' + '?$$app_token=' + data["api_key"] + "&$limit=1000"
+
+
+
+def request(url):
+	response = urllib.request.urlopen(url).read().decode("utf-8")
+	r = json.loads(response)
+	s = json.dumps(r, sort_keys=True, indent=2)
+	return (r , s)
+
+zipCarMemberCount = request(zipcarUrl1)
+#zipCarReservations = request(zipcarUrl2)
+#ticketsByAreaCode = request(ticketsUrl)
