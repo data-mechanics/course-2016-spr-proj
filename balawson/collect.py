ccac44792c51111e836d0aaf2fcf2ca40ee32fd9
ben lawson <balawson@bu.edu>
2016-02-28 11:35:54 -0500

    add twitter collection and .py viz

--- balawson/collect.py
+++ balawson/collect.py
@@ -48,6 +48,27 @@ download_fiter_and_insert(brightkite_filename, brightkite_url, 'brightkite', hea
 gowalla_filename = 'gowalla.txt.gz'
 gowalla_url      = 'https://snap.stanford.edu/data/loc-gowalla_totalCheckins.txt.gz'
 download_fiter_and_insert(gowalla_filename, gowalla_url, 'gowalla', header = ['user', 'time', 'lat', 'lng', 'location'])
+
+twitter_filename = '2016-02-25.sample.csv'
+twitter_url      = 'http://people.bu.edu/balawson/2016-02-25.sample.csv' 
+
+def download_and_insert(filename, url, collection_name, header = ['lat', 'location', 'lng', 'source', 'time', '_id', 'user']):
+    print('Begin downloading {0}'.format(filename))
+    response = requests.get(url, stream=True) 
+    with open(filename, "wb") as handle:
+       for data in response.iter_content(chunk_size=1024):
+           handle.write(data)
+    print('Finish downloading {0}'.format(filename))
+    ####read###
+    df = pd.read_csv('{0}'.format(filename), names = header)
+    df.drop('source', inplace=True, axis=1)
+    df =  df[ (df.lat < 42.445945) & (df.lat > 42.275086) & (df.lng > -71.194213) & (df.lng < -70.926301)]
+    print('Inserting {0} into mongo'.format(filename))
+    records = json.loads(df.T.to_json()).values()
+    repo.dropPermanent(collection_name)
+    repo.createPermanent(collection_name)
+    repo['balawson.' + collection_name].insert_many(records)
+download_and_insert(twitter_filename, twitter_url, 'twitter')
 endTime = datetime.datetime.now()
 
 # Create the provenance document describing everything happening
@@ -62,29 +83,41 @@ doc.add_namespace('dat', 'http://datamechanics.io/data/balawson/') # The data se
 doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
 doc.add_namespace('log', 'http://datamechanics.io/log#') # The event log.
 doc.add_namespace('snap', 'https://snap.stanford.edu/data/')
+doc.add_namespace('bal', 'http://people.bu.edu/balawson/')
 
 this_script = doc.agent('alg:collect', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
 brightkite_resource = doc.entity('snap:brightkite', {'prov:label':'SNAP: Standford Network Analysis Project - Brightkite', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'txtgz'})
 
 gowalla_resource = doc.entity('snap:gowalla', {'prov:label':'SNAP: Standford Network Analysis Project - Gowalla', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'txtgz'})
 
+twitter_resource = doc.entity('bal:twitter', {'prov:label':'Sample of Curated Tweet', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'csv'})
+
 get_brightkite = doc.activity('log:a'+str(uuid.uuid4()), startTime, endTime, {prov.model.PROV_TYPE:'ont:Retrieval'})
 get_gowalla = doc.activity('log:a'+str(uuid.uuid4()), startTime, endTime, {prov.model.PROV_TYPE:'ont:Retrieval'})
+get_twitter = doc.activity('log:a'+str(uuid.uuid4()), startTime, endTime, {prov.model.PROV_TYPE:'ont:Retrieval'})
 
 doc.wasAssociatedWith(get_brightkite, this_script)
 doc.wasAssociatedWith(get_gowalla, this_script)
+doc.wasAssociatedWith(get_twitter, this_script)
+
 doc.used(get_brightkite, brightkite_resource, startTime)
 doc.used(get_gowalla, gowalla_resource, startTime)
+doc.used(get_twitter, gowalla_twitter, startTime)
 
 brightkite_ent = doc.entity('dat:brightkite', {prov.model.PROV_LABEL:'Brightkite data', prov.model.PROV_TYPE:'ont:DataSet'})
 doc.wasAttributedTo(brightkite_ent, this_script)
 doc.wasGeneratedBy(brightkite_ent, get_brightkite, endTime)
 doc.wasDerivedFrom(brightkite_ent, brightkite_resource, get_brightkite, get_brightkite, get_brightkite)
 
-gowalla_ent = doc.entity('dat:found', {prov.model.PROV_LABEL:'Gowalla dataset', prov.model.PROV_TYPE:'ont:DataSet'})
+gowalla_ent = doc.entity('dat:gowalla', {prov.model.PROV_LABEL:'Gowalla dataset', prov.model.PROV_TYPE:'ont:DataSet'})
 doc.wasAttributedTo(gowalla_ent, this_script)
 doc.wasGeneratedBy(gowalla_ent, get_gowalla, endTime)
 doc.wasDerivedFrom(gowalla_ent, gowalla_resource, get_gowalla, get_gowalla, get_gowalla)
+
+twitter_ent = doc.entity('dat:twitter', {prov.model.PROV_LABEL:'Twitter dataset', prov.model.PROV_TYPE:'ont:DataSet'})
+doc.wasAttributedTo(twitter_ent, this_script)
+doc.wasGeneratedBy(twitter_ent, get_twitter, endTime)
+doc.wasDerivedFrom(twitter_ent, twitter_resource, get_twitter, get_twitter, get_twitter)
 
 repo.record(doc.serialize()) # Record the provenance document.
 #print(json.dumps(json.loads(doc.serialize()), indent=4))
