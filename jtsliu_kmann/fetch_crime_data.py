0eaa989a39212e1b18b578da26510e2d7e17c10f
Kyle Mann <kmann4376@gmail.com>
2016-03-01 21:01:39 -0500

    created basis for prov data in crime retrieval

--- jtsliu_kmann/fetch_crime_data.py
+++ jtsliu_kmann/fetch_crime_data.py
@@ -1,7 +1,9 @@
 import datetime
 import json
+import prov
 import pymongo
 import urllib.request
+import uuid
 
 # Open the file for interfacing with DB
 exec(open('../pymongo_dm.py').read())
@@ -34,6 +36,35 @@ while count == 50000:
 
 	repo['jtsliu_kmann.crime'].insert_many(clean)
 
+endTime = datetime.datetime.now()
+
+doc = prov.model.ProvDocument()
+doc.add_namespace('alg', 'http://datamechanics.io/algorithm/alice_bob/') # The scripts in / format.
+doc.add_namespace('dat', 'http://datamechanics.io/data/alice_bob/') # The data sets in / format.
+doc.add_namespace('ont', 'http://datamechanics.io/ontology#')
+doc.add_namespace('log', 'http://datamechanics.io/log#') # The event log.
+doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
+
+this_script = doc.agent('alg:fetch_crime_data', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension': 'py'})
+resource = doc.entity('bdp:ufcx-3fdn',
+	{'prov:label': 'Crime Incident Reports',
+	prov.model.PROV_TYPE: 'ont:DataResource', 'ont:Extension':'json'}
+)
+
+this_run = doc.activity(
+	'log:a'+str(uuid.uuid4()), startTime, endTime,
+	{prov.model.PROV_TYPE: 'ont:Retrieval', 'ont:Query': '?$limit=50000'}
+)
+
+doc.wasAssociatedWith(this_run, this_script)
+doc.used(this_run, resource, startTime)
+
+crime = doc.entity('dat:crime', {prov.model.PROV_LABEL:'Crime Incidents', prov.model.PROV_TYPE:'ont:DataSet'})
+doc.wasAttributedTo(crime, this_script)
+doc.wasGeneratedBy(crime, this_run, endTime)
+doc.wasDerivedFrom(crime, resource, this_run, this_run, this_run)
+
+repo.record(doc.serialize())
 
 
 
