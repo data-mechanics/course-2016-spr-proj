a7a4df5afdac53c3d4e314d530674937239d59ec
lapets <lapets@gmail.com>
2016-03-02 15:21:57 -0500

    Avoid merge conflict.

---
+++ README.md
@@ -0,0 +1,64 @@
+# course-2016-spr-proj-one
+Project repository for the first project in the Spring 2016 iteration of the Data Mechanics course at Boston University. ?
+
+In this project, you will implement components that obtain a few data sets from web services of your choice, insert them into the data set repository with appropriate provenance information, and combine them into at least two additional derived data sets (also with appropriate provenance information).
+
+**This project description will be updated as we continue work on the infrastructure.**
+
+## MongoDB infrastructure
+
+### Setting up
+
+We have committed setup scripts for a MongoDB database that will set up the database and collection management functions that ensure users sharing the project data repository can read everyone's collections but can only write to their own collections. Once you have installed your MongoDB instance, you can prepare it by first starting `mongod` _without authentication_:
+```
+mongod --dbpath "<your_db_path>"
+```
+Next, make sure your user directories (e.g., `alice_bob` if Alice and Bob are working together on a team) are present in the same location as the `setup.js` script, open a separate terminal window, and run the script:
+```
+mongo setup.js
+```
+Your MongoDB instance should now be ready. Stop `mongod` and restart it, enabling authentication with the `--auth` option:
+```
+mongod --auth --dbpath "<your_db_path>"
+```
+
+### Working on data sets with authentication
+
+With authentication enabled, you can start `mongo` on the repository (called `repo` by default) with your user credentials:
+```
+mongo repo -u alice_bob -p alice_bob --authenticationDatabase "repo"
+```
+However, you should be unable to create new collections using `db.createCollection()` in the default `repo` database created for this project:
+```
+> db.createCollection("EXAMPLE");
+{
+  "ok" : 0,
+  "errmsg" : "not authorized on repo to execute command { create: \"EXAMPLE\" }",
+  "code" : 13
+}
+```
+Instead, load the server-side functions so that you can use the customized `createTemp()` or `createPerm()` functions, which will create collections that can be read by everyone but written only by you:
+```
+> db.loadServerScripts();
+> var EXAMPLE = createPerm("EXAMPLE");
+```
+Notice that this function also prefixes the user name to the name of the collection (unless the prefix is already present in the name supplied to the function).
+```
+> EXAMPLE
+alice_bob.EXAMPLE
+> db.alice_bob.EXAMPLE.insert({value:123})
+WriteResult({ "nInserted" : 1 })
+> db.alice_bob.EXAMPLE.find()
+{ "_id" : ObjectId("56b7adef3503ebd45080bd87"), "value" : 123 }
+```
+For temporary collections that are only necessary during intermediate steps of of a computation, use `createTemp()`; for permanent collections that represent data that is imported or derived, use `createPerm()`.
+
+If you do not want to run `db.loadServerScripts()` every time you open a new terminal, you can use a `.mongorc.js` file in your home directory to store any commands or calls you want issued whenever you run `mongo`.
+
+## Python infrastructure
+
+To use PyMongo with the above interface, run the `pymongo_dm.py` script at the top of your modules or script:
+```
+exec(open('../pymongo_dm.py').read())
+```
+The script `alice_bob/example.py` is an example that illustrates how the wrappers can be used. It also provides a detailed example of how to record appropriate provenance information using the `prov` module.
---
+++ alice_bob/example.py
@@ -0,0 +1,83 @@
+import urllib.request
+import json
+import pymongo
+import prov.model
+import datetime
+import uuid
+
+# Until a library is created, we just use the script directly.
+exec(open('../pymongo_dm.py').read())
+
+# Set up the database connection.
+client = pymongo.MongoClient()
+repo = client.repo
+repo.authenticate('alice_bob', 'alice_bob')
+
+# Retrieve some data sets (not using the API here for the sake of simplicity).
+startTime = datetime.datetime.now()
+
+url = 'http://cs-people.bu.edu/lapets/591/examples/lost.json'
+response = urllib.request.urlopen(url).read().decode("utf-8")
+r = json.loads(response)
+s = json.dumps(r, sort_keys=True, indent=2)
+repo.dropPermanent("lost")
+repo.createPermanent("lost")
+repo['alice_bob.lost'].insert_many(r)
+
+url = 'http://cs-people.bu.edu/lapets/591/examples/found.json'
+response = urllib.request.urlopen(url).read().decode("utf-8")
+r = json.loads(response)
+s = json.dumps(r, sort_keys=True, indent=2)
+repo.dropPermanent("found")
+repo.createPermanent("found")
+repo['alice_bob.found'].insert_many(r)
+
+endTime = datetime.datetime.now()
+
+# Create the provenance document describing everything happening
+# in this script. Each run of the script will generate a new
+# document describing that invocation event. This information
+# can then be used on subsequent runs to determine dependencies
+# and "replay" everything. The old documents will also act as a
+# log.
+doc = prov.model.ProvDocument()
+doc.add_namespace('alg', 'http://datamechanics.io/algorithm/alice_bob/') # The scripts in <folder>/<filename> format.
+doc.add_namespace('dat', 'http://datamechanics.io/data/alice_bob/') # The data sets in <user>/<collection> format.
+doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
+doc.add_namespace('log', 'http://datamechanics.io/log#') # The event log.
+doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
+
+this_script = doc.agent('alg:example', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+resource = doc.entity('bdp:wc8w-nujj', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+get_found = doc.activity('log:a'+str(uuid.uuid4()), startTime, endTime)
+get_lost = doc.activity('log:a'+str(uuid.uuid4()), startTime, endTime)
+doc.wasAssociatedWith(get_found, this_script)
+doc.wasAssociatedWith(get_lost, this_script)
+doc.usage(get_found, resource, startTime, None,
+        {prov.model.PROV_TYPE:'ont:Retrieval',
+         'ont:Query':'?type=Animal+Found&$select=type,latitude,longitude,OPEN_DT'
+        }
+    )
+doc.usage(get_lost, resource, startTime, None,
+        {prov.model.PROV_TYPE:'ont:Retrieval',
+         'ont:Query':'?type=Animal+Lost&$select=type,latitude,longitude,OPEN_DT'
+        }
+    )
+
+lost = doc.entity('dat:lost', {prov.model.PROV_LABEL:'Animals Lost', prov.model.PROV_TYPE:'ont:DataSet'})
+doc.wasAttributedTo(lost, this_script)
+doc.wasGeneratedBy(lost, get_lost, endTime)
+doc.wasDerivedFrom(lost, resource, get_lost, get_lost, get_lost)
+
+found = doc.entity('dat:found', {prov.model.PROV_LABEL:'Animals Found', prov.model.PROV_TYPE:'ont:DataSet'})
+doc.wasAttributedTo(found, this_script)
+doc.wasGeneratedBy(found, get_found, endTime)
+doc.wasDerivedFrom(found, resource, get_found, get_found, get_found)
+
+repo.record(doc.serialize()) # Record the provenance document.
+#print(json.dumps(json.loads(doc.serialize()), indent=4))
+open('plan.json','w').write(json.dumps(json.loads(doc.serialize()), indent=4))
+print(doc.get_provn())
+repo.logout()
+
+## eof
