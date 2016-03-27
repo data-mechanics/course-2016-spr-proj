5aa57a9dbc4e7ddbf605c782f167a904e8498a2f
root <root@ip-172-31-16-87.ec2.internal>
2016-03-27 21:28:17 +0000

    Fix some issues with script.

--- nlouie_thsu8/script.py
+++ nlouie_thsu8/script.py
@@ -61,9 +61,20 @@ def insert_to_db(repo, s):
     url = 'https://data-mechanics.s3.amazonaws.com/nlouie_thsu8/data/' + s + '.json'
     response = urllib.request.urlopen(url).read().decode("utf-8")
     r = json.loads(response)
-    s = json.dumps(r, sort_keys=True, indent=2)
+    
+    # The line below was causing an error.
+    #r = json.dumps(r, sort_keys=True, indent=2)
+
     repo.dropPermanent(s)
     repo.createPermanent(s)
+
+    # Convert to valid JSON dictionary if necessary.
+    for i in range(len(r)):
+        if type(r[i]) == list:
+            d = {}
+            d[r[i][0]] = r[i][1]
+            r[i] = d
+
     repo['nlouie_thsu8.' + s].insert_many(r)
 
 # add to mongo database and generate prov
