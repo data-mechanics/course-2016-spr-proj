6512709d98e1b91a1243f6afc6c8f6b78b829247
nlouie <louie.nick@yahoo.com>
2016-04-11 18:28:56 -0400

    moved main javascript code to js/script.js

---
+++ nlouie_thsu8/js/script.js
@@ -0,0 +1,38 @@
+/**
+ * Created by nlouie on 4/11/16.
+ */
+// http://leafletjs.com/examples/geojson.html
+var count = 0;
+
+var streetlights = L.map('streetlights').setView(L.latLng(42.3562559, -71.1), 10);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+    maxZoom: 18,
+    attribution: '',
+    id: 'mapbox.light'
+}).addTo(streetlights);
+
+function onEachFeature(feature, layer) {
+    // does this feature have a property named popupContent?
+    if (feature.properties && feature.properties.popupContent) {
+        layer.bindPopup(feature.properties.popupContent);
+    }
+    count = count + 1;
+}
+
+var geojsonMarkerOptions = {
+    radius: 2,
+    fillColor: "#ff7800",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+};
+
+L.geoJson(lights, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, geojsonMarkerOptions);
+    },
+    onEachFeature: onEachFeature
+}).addTo(streetlights);
+
