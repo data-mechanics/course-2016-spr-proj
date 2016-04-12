c6ef8c51d34cc1539e7aecc5232f774ab40e9ba4
Erik Brakke <eriksbrakke@gmail.com>
2016-04-12 09:25:26 -0400

    refactored to allow an auth.json file when necessary.  Removed controller.py file

---
+++ ebrakke_twaltze/accident_filter.py
@@ -0,0 +1,48 @@
+import prov.model
+import pymongo
+import datetime
+import time
+import uuid
+import sys
+import subprocess
+
+exec(open('../pymongo_dm.py').read())
+
+client = pymongo.MongoClient()
+repo = client.repo
+repo.authenticate('ebrakke_twaltze', 'ebrakke_twaltze')
+
+
+def accident_filter():
+    start_time = datetime.datetime.now()
+    try:
+        subprocess.check_output('mongo repo -u ebrakke_twaltze -p ebrakke_twaltze --authenticationDatabase "repo" accidentFilter.js', shell=True)
+        end_time = datetime.datetime.now()
+        do_prov(start_time, end_time)
+    except subprocess.CalledProcessError as e:
+        print(e.output)
+
+def do_prov(start_time=None, end_time=None):
+	doc = prov.model.ProvDocument()
+	doc.add_namespace('alg', 'http://datamechanics.io/algorithm/ebrakke_twaltze/')
+	doc.add_namespace('dat', 'http://datamechanics.io/data/ebrakke_twaltze/')
+	doc.add_namespace('ont', 'http://datamechanics.io/ontology#')
+	doc.add_namespace('log', 'http://datamechanics.io/log#')
+
+	script = doc.agent('alg:accidentFilter', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension': 'js'})
+	crime = doc.entity('dat:crimeReports', {prov.model.PROV_LABEL:'Crime Reports', prov.model.PROV_TYPE:'ont:DataSet'})
+
+	filter_accidents = doc.activity('log:a' + str(uuid.uuid4()), start_time, end_time, {prov.model.PROV_LABEL: 'Filter service calls for potholes'})
+	doc.wasAssociatedWith(filter_accidents, script)
+	doc.usage(filter_accidents, crime, start_time, None, {prov.model.PROV_TYPE:'ont:Computation'})
+
+	accidents = doc.entity('dat:accidents', {prov.model.PROV_LABEL:'Accidents', prov.model.PROV_TYPE:'ont:DataSet'})
+	doc.wasAttributedTo(accidents, script)
+	doc.wasGeneratedBy(accidents, filter_accidents, end_time)
+	doc.wasDerivedFrom(accidents, crime, filter_accidents, filter_accidents, filter_accidents)
+
+	repo.record(doc.serialize())
+	print(doc.get_provn())
+
+if __name__ == '__main__':
+    accident_filter()
--- ebrakke_twaltze/calculate_danger_levels.py
+++ ebrakke_twaltze/calculate_danger_levels.py
@@ -1,10 +1,11 @@
+import pymongo
+import prov.model
+import datetime
+import uuid
+
 from DangerZones import Pothole
 from DangerZones import Accident
 from DangerZones import Construction
-
-import pymongo
-import prov.model
-import sys
 
 exec(open('../pymongo_dm.py').read())
 
@@ -12,36 +13,75 @@ client = pymongo.MongoClient()
 repo = client.repo
 repo.authenticate('ebrakke_twaltze', 'ebrakke_twaltze')
 
-# Collection to store the danger levels to
-collection = 'dangerLevels'
-repo.dropPermanent(collection)
-repo.createPermanent(collection)
+def calculate_danger_levels():
+    start_time = datetime.datetime.now()
 
-datasets = ['potholes', 'construction', 'accidents']
-data = []
-for dataset in datasets:
-    dataset = repo['ebrakke_twaltze.' + dataset].find()
 
-    for d in dataset:
-        dangerZoneType = d.get('type')
+    # Collection to store the danger levels to
+    collection = 'dangerLevels'
+    repo.dropPermanent(collection)
+    repo.createPermanent(collection)
 
-        # Determine the type of zone
-        if dangerZoneType == 'pothole':
-            dangerZone = Pothole(d.get('latitude'), d.get('longitude'), d.get('report_dt'), d.get('status'))
-        elif dangerZoneType == 'construction':
-            dangerZone = Construction(d.get('latitude'), d.get('longitude'), d.get('expected_close_date'), d.get('num_sidewalk_plates'), d.get('num_road_plates'))
-        elif dangerZoneType == 'accident':
-            dangerZone = Accident(d.get('latitude'), d.get('longitude'), d.get('report_dt'))
+    datasets = ['potholes', 'construction', 'accidents']
+    data = []
+    for dataset in datasets:
+        dataset = repo['ebrakke_twaltze.' + dataset].find()
 
-        # Get the danger level for this data point
-        dangerLevel = dangerZone.calculateDangerLevel()
+        for d in dataset:
+            dangerZoneType = d.get('type')
 
-        # Insert into db
-        data.append({
-            'lat': d.get('latitude'),
-            'lng': d.get('longitude'),
-            'dangerLevel': dangerLevel,
-            'type': d.get('type')
-        })
+            # Determine the type of zone
+            if dangerZoneType == 'pothole':
+                dangerZone = Pothole(d.get('latitude'), d.get('longitude'), d.get('report_dt'), d.get('status'))
+            elif dangerZoneType == 'construction':
+                dangerZone = Construction(d.get('latitude'), d.get('longitude'), d.get('expected_close_date'), d.get('num_sidewalk_plates'), d.get('num_road_plates'))
+            elif dangerZoneType == 'accident':
+                dangerZone = Accident(d.get('latitude'), d.get('longitude'), d.get('report_dt'))
 
-repo['ebrakke_twaltze.' + collection].insert_many(data)
+            # Get the danger level for this data point
+            dangerLevel = dangerZone.calculateDangerLevel()
+
+            # Insert into db
+            data.append({
+                'lat': d.get('latitude'),
+                'lng': d.get('longitude'),
+                'dangerLevel': dangerLevel,
+                'type': d.get('type')
+            })
+
+    repo['ebrakke_twaltze.' + collection].insert_many(data)
+    end_time = datetime.datetime.now()
+
+    do_prov(start_time, end_time)
+
+def do_prov(start_time, end_time):
+    doc = prov.model.ProvDocument()
+    doc.add_namespace('alg', 'http://datamechanics.io/algorithm/ebrakke_twaltze/')
+    doc.add_namespace('dat', 'http://datamechanics.io/data/ebrakke_twaltze/')
+    doc.add_namespace('ont', 'http://datamechanics.io/ontology#')
+    doc.add_namespace('log', 'http://datamechanics.io/log#')
+
+    script = doc.agent('alg:calculate_danger_levels', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension': 'py'})
+    construction = doc.entity('dat:construction', {prov.model.PROV_LABEL:'Construction', prov.model.PROV_TYPE:'ont:DataSet'})
+    potholes = doc.entity('dat:potholes', {prov.model.PROV_LABEL:'Potholes', prov.model.PROV_TYPE:'ont:DataSet'})
+    accidents = doc.entity('dat:accidents', {prov.model.PROV_LABEL:'Accidents', prov.model.PROV_TYPE:'ont:DataSet'})
+
+    calc_danger = doc.activity('log:a' + str(uuid.uuid4()), start_time, end_time, {prov.model.PROV_LABEL: 'Map each point to a danger level'})
+    doc.wasAssociatedWith(calc_danger, script)
+
+    doc.usage(calc_danger, potholes, start_time, None, {prov.model.PROV_TYPE:'ont:Computation'})
+    doc.usage(calc_danger, construction, start_time, None, {prov.model.PROV_TYPE:'ont:Computation'})
+    doc.usage(calc_danger, accidents, start_time, None, {prov.model.PROV_TYPE:'ont:Computation'})
+
+    danger_levels = doc.entity('dat:dangerLevels', {prov.model.PROV_LABEL: 'Danger Levels', prov.model.PROV_TYPE:'ont:DataSet'})
+    doc.wasAttributedTo(danger_levels, script)
+    doc.wasGeneratedBy(danger_levels, calc_danger, end_time)
+    doc.wasDerivedFrom(danger_levels, potholes, calc_danger, calc_danger, calc_danger)
+    doc.wasDerivedFrom(danger_levels, construction, calc_danger, calc_danger, calc_danger)
+    doc.wasDerivedFrom(danger_levels, accidents, calc_danger, calc_danger, calc_danger)
+
+    repo.record(doc.serialize())
+    print(doc.get_provn())
+
+if __name__ == '__main__':
+    calculate_danger_levels()
