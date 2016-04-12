7ab0870ff5216b9787fa25bd0b5c94f0819bbca8
tpacius <tpacius@bu.edu>
2016-04-12 14:24:35 -0400

    Eliminated redundancies in databaseIntersectionOp.py and added provenance data

--- jmuru1_joshmah_tpacius/databaseIntersectionOp.py
+++ jmuru1_joshmah_tpacius/databaseIntersectionOp.py
@@ -91,8 +91,6 @@ Y = Ynew
 X = intersect(Xnew,Ynew)
 Y = intersect(Ynew,Xnew)
 
-
-
 #Number of hospitals on streets that have intersections
 X = dict((x,X.count(x)) for x in set(X))
 # print(X)
@@ -166,35 +164,4 @@ open('plan.json','w').write(json.dumps(json.loads(doc.serialize()), indent=4))
 print(doc.get_provn())
 repo.logout()
 
-
-#Code from Lecture Notes
-
-def union(R, S):
-    return R + S
-
-def difference(R, S):
-    return [t for t in R if t not in S]
-
-def intersect(R, S):
-    return [t for t in R if t in S]
-
-def project(R, p):
-    return [p(t) for t in R]
-
-def select(R, s):
-    return [t for t in R if s(t)]
- 
-def product(R, S):
-    return [(t,u) for t in R for u in S]
-
-def aggregate(R, f):
-    keys = {r[0] for r in R}
-    return [(key, f([v for (k,v) in R if k == key] + [])) for key in keys]
-    
-def map(f, R):
-    return [t for (k,v) in R for t in f(k,v)]
-    
-def reduce(f, R):
-    keys = {k for (k,v) in R}
-    return [f(k1, [v for (k2,v) in R if k1 == k2]) for k1 in keys]
 ## eof
