56b230fa336674d24845b4be6040e1896a841793
mcculleydj <mcculleydj@gmail.com>
2016-04-13 19:16:34 -0400

    Project 2 Submission

--- djmcc_jasper/reset.py
+++ djmcc_jasper/reset.py
@@ -1,18 +1,29 @@
+###############################################
+# Title:   reset.py
+# Authors: Daren McCulley and Jasper Burns
+# Purpose: Database wipe and re-initialization 
+###############################################
+
 def run(repo):
 
-	repo.dropPermanent("assessments_2014")
-	repo.createPermanent("assessments_2014")
-	repo.dropPermanent("assessments_2015")
-	repo.createPermanent("assessments_2015")
-	repo.dropPermanent("approved_permits")
-	repo.createPermanent("approved_permits")
-	repo.dropPermanent("neighborhood_zoning")
-	repo.createPermanent("neighborhood_zoning")
-	repo.dropPermanent("residential_centers")
-	repo.createPermanent("residential_centers")
-	repo.dropPermanent("commercial_centers")
-	repo.createPermanent("commercial_centers")
-	repo.dropPermanent("ptypes")
-	repo.createPermanent("ptypes")
+	print('Dropping collections...')
+	repo.dropPermanent('apartments')
+	repo.dropPermanent('collapsed_apartments')
+	repo.dropPermanent('filtered_apartments')
+	repo.dropPermanent('assessments')
+	repo.dropPermanent('filtered_assessments')
+	repo.dropPermanent('merged_assessments')
+	repo.dropPermanent('joined')
+	repo.dropPermanent('web_data')
+
+	print('Creating collections...')
+	repo.createPermanent('apartments')
+	repo.createPermanent('collapsed_apartments')
+	repo.createPermanent('filtered_apartments')
+	repo.createPermanent('assessments')
+	repo.createPermanent('filtered_assessments')
+	repo.createPermanent('merged_assessments')
+	repo.createPermanent('joined')
+	repo.createPermanent('web_data')
 
 #EOF
