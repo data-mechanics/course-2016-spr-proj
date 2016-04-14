34b198f53c50b6ab49be02cd5637dda867a18007
Ben Lawson <balawson@bu.edu>
2016-04-13 23:19:32 -0400

    add capture/recapture and clean up xml conversion

--- balawson/xml_to_dataframe.py
+++ balawson/xml_to_dataframe.py
@@ -15,7 +15,7 @@ exec(open('../pymongo_dm.py').read())
 ###############################################################
 
 startTime  = datetime.datetime.now()
-roads, lat, lng, population = [], [] , [] , [] #lists to store the information
+roads, lat, lng, population, users = [], [] , [] , [] , [] #lists to store the information
 e = xmltree.parse('BostonNetwork/outputs/roadnetwork.xml').getroot()
 for intersection in e.findall('intersection'):
     children = intersection.getchildren() 
@@ -28,9 +28,11 @@ for intersection in e.findall('intersection'):
                 r += ', ' + ( child.get('name'))
         elif child.get('num') :
             pop = child.get('num')
+        elif child.get('ulist') :
+            user = child.get('ulist')
         else:
             pass
-
+    users += [user]
     lat += [intersection.get('lat')]
     lng += [intersection.get('long')]
     roads.append(r)
@@ -43,19 +45,25 @@ for intersection in e.findall('intersection'):
 d = {'roads'    : pd.Series(roads) ,
     'lat'       : pd.Series(lat)      ,
     'lng'       : pd.Series(lng)      ,
-    'population': pd.Series(population)
+    'population': pd.Series(population) , 
+    'users': pd.Series(users) , 
     }
 
 df = pd.DataFrame(d)
-df.to_csv('intersectionspopulation.csv')
+#df.to_csv('intersectionspopulation.csv')
 
 ###############################################################
-####    save results to temp csv
+####    dump dataframe
 ###############################################################
 client = pymongo.MongoClient()
 repo = client.repo
 repo.authenticate('balawson', 'balawson')
 
+collection_name = 'twitter-intersection'
+records = json.loads(df.T.to_json()).values()
+repo.dropPermanent(collection_name)
+repo.createPermanent(collection_name)
+repo['balawson.' + collection_name].insert_many(records)
 
 endTime = datetime.datetime.now()
 ###############################################################
@@ -66,19 +74,15 @@ doc.add_namespace('alg', 'http://datamechanics.io/algorithm/balawson/') # The sc
 doc.add_namespace('dat', 'http://datamechanics.io/data/balawson/') # The data sets in <user>/<collection> format.
 doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
 doc.add_namespace('log', 'http://datamechanics.io/log#') # The event log.
-doc.add_namespace('snap', 'https://snap.stanford.edu/data/')
+#doc.add_namespace('snap', 'https://snap.stanford.edu/data/')
 doc.add_namespace('bal', 'http://people.bu.edu/balawson/')
 
 this_script = doc.agent('alg:compare', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
-brightkite_resource = doc.entity('snap:brightkite', {'prov:label':'SNAP: Standford Network Analysis Project - Brightkite', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'txtgz'})
-gowalla_resource = doc.entity('snap:gowalla', {'prov:label':'SNAP: Standford Network Analysis Project - Gowalla', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'txtgz'})
-twitter_resource = doc.entity('bal:twitter', {'prov:label':'Sample of Curated Tweet', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'csv'})
+#brightkite_resource = doc.entity('snap:brightkite', {'prov:label':'SNAP: Standford Network Analysis Project - Brightkite', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'txtgz'})
+#gowalla_resource = doc.entity('snap:gowalla', {'prov:label':'SNAP: Standford Network Analysis Project - Gowalla', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'txtgz'})
+twitter_xml = doc.entity('bal:twitter', {'prov:label':'Derived intermediate state of curated Tweets', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'xml'})
 
-viz_brightkite = doc.activity('log:a'+str(uuid.uuid4()), startTime, endTime, {prov.model.PROV_TYPE:'ont:Computation'})
-
-viz_gowalla = doc.activity('log:a'+str(uuid.uuid4()), startTime, endTime, {prov.model.PROV_TYPE:'ont:Computation'})
-
-viz_twitter = doc.activity('log:a'+str(uuid.uuid4()), startTime, endTime, {prov.model.PROV_TYPE:'ont:Computation'})
+#viz_brightkite = doc.activity('log:a'+str(uuid.uuid4()), startTime, endTime, {prov.model.PROV_TYPE:'ont:Computation'})
 
 doc.wasAssociatedWith(viz_brightkite, this_script)
 doc.wasAssociatedWith(viz_gowalla, this_script)
