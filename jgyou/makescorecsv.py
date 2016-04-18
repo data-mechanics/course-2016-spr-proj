87092a17cd054ef444e3e61ecb77178d3fe62185
jgyou-bu <jgyou@bu.edu>
2016-04-18 18:42:10 -0400

    updated makescorecsv

--- jgyou/makescorecsv.py
+++ jgyou/makescorecsv.py
@@ -1,3 +1,10 @@
+'''
+makescorecsv.py 
+
+Script that creates csv file based on score output and blockgroup data 
+from Housing and Urban Development department. Information in this CSV file
+is basis for the scatterplot.
+'''
 from urllib import request, parse
 import json
 import pymongo
@@ -8,61 +15,121 @@ import time
 import csv
 import re
 
-# retrieve certain elements from the Location Affordability Index by Block dataset
+exec(open('../pymongo_dm.py').read())
 
-with open("lai_data_14460_blkgrps.csv") as f2:
-	laiinfo = csv.reader(f2)
-	laiarr = []
-	headers = next(laiinfo, None)
-	for row in laiinfo:
-		censusblock = int(row[headers.index("blkgrp")].replace("'", ""))
-		rai = float(row[headers.index("retail_access_index")])
-		laiarr.append((censusblock, rai))
-	f2.close()
+# prov document uses the original input data set for documentation purposes
+# to retain origin of data; however, actual data set retrieved from datamechanics
+# directories
+def make_provdoc(repo, run_id, startTime, endTime):
+    provdoc = prov.model.ProvDocument()
+    provdoc.add_namespace('alg', 'http://datamechanics.io/algorithm/' + user + '/') # The scripts in <folder>/<filename> format.
+    provdoc.add_namespace('dat', 'http://datamechanics.io/data/' + user + '/') # The data sets in <user>/<collection> format.
+    provdoc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
+    provdoc.add_namespace('log', 'http://datamechanics.io/log#') # The event log.
+    provdoc.add_namespace('lai', 'http://lai.locationaffordability.info//download_csv.php')        # Location Affordability Index
 
-	# then using score output, get addr, censusblock, etc.
-	
-	with open("scores.json") as f:
-		scores = json.loads(f.read())
-		output = [[None for j in range(5)] for i in range(len(scores) + 1)]
-		for i, s in enumerate(scores):
-			addr = s['address']
-			censusblock = s['census_block']
-			score = s['score']
-			output[i + 1][0] = addr
-			output[i + 1][1] = censusblock
-			output[i + 1][3] = score
+    this_script = provdoc.agent('alg:makescorecsv', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+    input1 = provdoc.entity('lai:', {prov.model.PROV_LABEL:'Senior Services', prov.model.PROV_TYPE:'ont:DataSet', 'ont:Extension': 'json'})
+    input2 = provdoc.entity('dat:scores', {prov.model.PROV_LABEL:'Distance Scores', prov.model.PROV_TYPE:'ont:DataSet', 'ont:Extension': 'json'})
+    output1 = provdoc.entity('dat:plot', {prov.model.PROV_LABEL:'Plot LAI CSV Data', prov.model.PROV_TYPE:'ont:DataSet', 'ont:Extension': 'csv'})
+    
+    this_run = provdoc.activity('log:a'+run_id, startTime, endTime)
+    provdoc.wasAssociatedWith(this_run, this_script)
+    provdoc.used(this_run, input2, startTime, None, {prov.model.PROV_TYPE:'ont:Computation'})
+    provdoc.used(this_run, input1, startTime, None,\
+        {prov.model.PROV_TYPE:'ont:Retrieval', 'ont:Query': "cbsa=14460&geography=blkgrp"})
 
-			if re.search("[0-9]{5,5}", addr):
-				zipcode = re.findall("[0-9]{5,5}", addr)[0]
-			else:
-				zipcode = None
+    provdoc.wasAttributedTo(output1, this_script)
+    provdoc.wasDerivedFrom(output1, input1, this_run, this_run, this_run)
+    provdoc.wasDerivedFrom(output1, input2, this_run, this_run, this_run)
+    provdoc.wasGeneratedBy(output1, this_run, endTime)
 
-			output[i + 1][2] = zipcode
+    if startTime == None:
+        plan = open('plan.json','r')
+        docModel = prov.model.ProvDocument()
+        doc = docModel.deserialize(plan)
+        doc.update(provdoc)
+        plan.close()
+        plan = open('plan.json', 'w')
+        plan.write(json.dumps(json.loads(doc.serialize()), indent=4))
+        plan.close()
+    else:
+        repo.record(provdoc.serialize()) 
 
-			result = [rai for (cb, rai) in laiarr if str(cb) in str(censusblock)]
 
-			# if there is no matching census block info, take 
-			if len(result) == 0:
-				result2 = [rai for (cb, rai) in laiarr if str(cb)[0:len(str(cb))-1] in str(censusblock)]
-				if len(result2) != 0:
-					output[i + 1][4] = result2[0]
-			else:
-				output[i + 1][4] = result[0]
+# retrieve certain elements from the dataset "Location Affordability Index by Census Block Group"
+client = pymongo.MongoClient()
+repo = client.repo
 
-		f.close()
+with open("auth.json") as f:
+    auth = json.loads(f.read())
+    user = auth['user']
 
-		output[0] = ["address", "censusblock", "zipcode", "score", "rai"]
+    repo.authenticate(user, user)
 
-		# write out this information to csv file to be used in d3 plots
+    startTime = datetime.datetime.now()
 
-		with open("plot.csv", "w") as f3:
-			plt = csv.writer(f3)
-			plt.writerows(output)
-			f3.close()
-			
-		
-	
+    with request.urlopen("http://datamechanics.io/data/jgyou/lai_data_14460_blkgrps.csv") as f2:
+        laiinfo = csv.reader(f2.read().decode("utf-8").splitlines())
+        laiarr = []
+        headers = next(laiinfo, None)
+        
+        for row in laiinfo:
+            censusblock = int(row[headers.index("blkgrp")].replace("'", ""))
+            rai = float(row[headers.index("retail_access_index")])
+            laiarr.append((censusblock, rai))
+        f2.close()
+
+        # then using score output, get addr, censusblock, etc.
+        
+        with open("scores.json") as f4:
+            scores = json.loads(f4.read())
+            output = [[None for j in range(5)] for i in range(len(scores) + 1)]
+            for i, s in enumerate(scores):
+                addr = s['address']
+                censusblock = s['census_block']
+                score = s['score']
+                output[i + 1][0] = addr
+                output[i + 1][1] = censusblock
+                output[i + 1][3] = score
+
+                if re.search("[0-9]{5,5}", addr):
+                    zipcode = re.findall("[0-9]{5,5}", addr)[0]
+                else:
+                    zipcode = None
+
+                output[i + 1][2] = zipcode
+
+                result = [rai for (cb, rai) in laiarr if str(cb) in str(censusblock)]
+
+                # if there is no matching census block info, take 
+                if len(result) == 0:
+                    result2 = [rai for (cb, rai) in laiarr if str(cb)[0:len(str(cb))-1] in str(censusblock)]
+                    if len(result2) != 0:
+                        output[i + 1][4] = result2[0]
+                else:
+                    output[i + 1][4] = result[0]
+
+            f4.close()
+
+            # header for csv
+            output[0] = ["address", "censusblock", "zipcode", "score", "rai"]
+
+            # write out this information to csv file to be used in d3 plots
+
+            with open("plot.csv", "w") as f3:
+                plt = csv.writer(f3)
+                plt.writerows(output)
+                f3.close()
+
+                endTime = datetime.datetime.now()
+
+                run_id = str(uuid.uuid4())
+                make_provdoc(repo, run_id, startTime, endTime)
+                make_provdoc(repo, run_id, None, None)
+            
+        
+    
 
 
 
--- jgyou/runallscripts.py
+++ jgyou/runallscripts.py
@@ -1,20 +1,9 @@
 '''
 runallscripts.py
 
-
+central script to run all other scripts
 
 '''
-
-
-from random import uniform
-from statistics import median
-
-# supplementary functions
-exec(open("convertcoordinates.py").read())  # something wrong with makeProvCensus
-exec(open("findnearest.py").read())		# needs prov, which is made tricky by the use of multiple collections
-exec(open("generatemiscprov.py").read())
-exec(open('../pymongo_dm.py').read())
-
 
 # get some data sets
 exec(open("retrievehospitals.py").read())
@@ -24,108 +13,15 @@ exec(open("retrieveservices.py").read())
 #exec(open("retrievepharmacies.py").read())		# needs to be changed to find nearest X pharmacies
 exec(open("retrievezillow.py").read())			# currently have not figured out way to incorporate into score, also update retrieval query prov accordingly
 
+# calculate scores
+exec(open("scorecoordinates.py").read())
+# findnearest, score coordinates both need prov
 
-# highly unscientific function to generate initial coordinates
-# essentially - using a box formed by the following approximate coordinates,
-# a series of points are chosen from within the box.
-# The box's coordinates were derived from manual retrieval on Google Maps.
-# in future versions of this project, points will be chosen using a geojson
-# polygon file of Boston neighborhoods to produce points that are less biased
-# TR Downtown
-# Boston, MA
-# 42.351454, -71.055614
+# create csv for visualization in D3
+exec(open("makescorecsv.py").read())
+			
 
-# tL Fenway/Kenmore
-# Boston, MA
-# 42.349171, -71.105396
-
-# BL Southern Mattapan
-# Boston, MA
-# 42.278593, -71.105052
-
-# BR Lower East Mills / Cedar Grove
-# Boston, MA
-# 42.279609, -71.053554
-
-
-def generatePoints(n):
-	(topLeftLat, topLeftLon) = (42.349171, -71.105396)
-	(topRightLat, topRightLon) = (42.351454, -71.055614)
-	(bottomLeftLat, bottomLeftLon) = (42.278593, -71.105052)
-	(bottomRightLat, bottomRightLon) = (42.279609, -71.053554)
-
-	x1 = min(topLeftLon, bottomLeftLon)
-	x2 = max(topRightLon, bottomRightLon)
-	y1 = min(bottomRightLat, bottomLeftLat)
-	y2 = max(topRightLat, topLeftLat)
-
-	# 5-decimal place lat-long coordinates randomly created
-	return [(round(uniform(y1, y2), 5), round(uniform(x1, x2), 5)) for x in range(n)]
-
-
-def assignStopWeight(wheelchairstatus):
-	if wheelchairstatus == 0:
-		return 1.0
-	return 0.9
-
-
-
-client = pymongo.MongoClient()
-repo = client.repo
-
-with open("auth.json") as f:
-	auth = json.loads(f.read())
-	user = auth['user']
-
-	repo.authenticate(user, user)
-
-	startTime = datetime.datetime.now()
-
-	#################
-
-	startingLocations = generatePoints(20)
-
-	weights = [1, 3, 6]
-
-	allscores = []
-
-	for startLocation in startingLocations:
-
-		(startlat, startlon) = startLocation
-		
-		fip = getCensus(repo, user, startLocation)  # problematic line
-		
-		(addr, neigh, zipcode) = getAddress(repo, user, startLocation)
-
-		if zipcode == -1:
-			# zipcode not found by geocoder due to incomplete data, look for zipcode based on neighborhood
-			continue
-
-		
-		distHospital = findClosestCoordinate(repo, user + ".hospitals", startLocation)
-
-	
-		# when scoring mbta stops, should "reward" stops that have wheelchair access
-		nearerStops = boundedRadiusMBTA(repo, user + ".mbtaStops", startLocation, 3.0)
-		if len(nearerStops) != 0:
-			medianMBTA = median([b*assignStopWeight(f) for (a,b, c, d, e, f) in nearerStops])
-		else:
-			medianMBTA = 3.0
-
-		distCenter = findClosestCoordinate(repo, user + '.servicecenters', startLocation)
-
-		score = weights[0]*distHospital + weights[2]*medianMBTA + weights[1]*distCenter
-
-		allscores.append( {"address": addr, "census_block": fip, "score": score, "longitude": startlon, "latitude": startlat})
-
-
-		with open('scores.json', 'w') as output_scores:
-			output_scores.write(json.dumps(allscores, indent=4))
-
-			# needs prov data
-
-
-			repo.logout()
+			
 
 
 
