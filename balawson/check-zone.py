76e5c515ea84a217b388ae6beb00e7aa9e3d5e1f
Ben L <balawson@bu.edu>
2016-04-23 21:12:00 -0400

    update popular intersections

--- balawson/check-zone.py
+++ balawson/check-zone.py
@@ -86,6 +86,7 @@ def color_map(df):
 def make_patch(color, label):
     return mpatches.Patch(color=color, label=label)
 
+tweets = tweets.convert_objects(convert_numeric=True) #TODO this is getting deprenciated
 tweets['total'] = tweets.apply(lambda d: countpeople( d), axis=1)
 tweets.drop(['0', '1', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19','2', '20', '21', '22', '23', '3', '4', '5', '6', '7', '8', '9', '_id',], inplace=True, axis=1)
 tweets['lat'] = tweets.lat.apply(lambda d: float(d))
@@ -104,18 +105,19 @@ leg = []
 for key in cmap:
     leg.append(make_patch(label=key, color=cmap[key]))
 
-
-sorted_tweets[:25].total.plot(kind='bar', color=colorful, figsize=(10,15))
-plt.legend(handles=leg, loc='center left', bbox_to_anchor=(1, 0.5))
-plt.title("Top 25 most population intersections")
-plt.savefig('img/popularintersections.png')
-
 plt.gcf().subplots_adjust(bottom=0.22)
 plt.xlabel('Neighborhood')
 plt.ylabel('Number of Intersections')
 plt.title('Intersections by Neighborhood')
 tweets.neighborhoods.value_counts().plot(kind='bar')
 plt.savefig('img/numberofintersections.png')
+
+sorted_tweets[:25].total.plot(kind='bar', color=colorful, figsize=(10,10))
+#plt.legend(handles=leg, loc='center left', bbox_to_anchor=(1, 0.5))
+plt.legend(handles=leg)
+plt.title("Top 25 most popular intersections")
+plt.savefig('img/popularintersections.png')
+
 
 ###############################################################
 ####    save results       
