0ac9f0caf19d5dffcb46ecf3d634ba708c8293ad
dianaantipode <chang.diana.gao@gmail.com>
2016-05-24 16:05:49 -0400

    running script

--- balawson/run.sh
+++ balawson/run.sh
@@ -7,3 +7,6 @@ python3 cluster.py
 python3 capture-recapture.py
 
 python3 data-viz.py
+
+#visualization will be achieved using "ipython notebook data-viz.ipynb"
+#and latex files will be compiled using "sudo apt-get install latexmk" and "latexmk  -pvc -pdf report.tex"
