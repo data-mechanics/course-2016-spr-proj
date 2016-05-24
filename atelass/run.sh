14dab77954cc885fe0fb8100c65e7d5c0c952da0
dianaantipode <chang.diana.gao@gmail.com>
2016-05-24 15:20:15 -0400

    sequence of running all files

---
+++ atelass/run.sh
@@ -0,0 +1,10 @@
+
+python3 data_retrieval.py ../auth.json
+
+python3 data_manipulation.py 
+
+python3 correlation.py
+
+python3 scoring.py
+
+python mapping.py
--- ciestu12_sajarvis/run.sh
+++ ciestu12_sajarvis/run.sh
@@ -4,7 +4,7 @@ python3 get_boarding_info.py #gives green_line_boarding_counts
 
 python3 make_stop_gps_db.py #gives t_stop_locations
 
-python3 make_walk_dist_db.py #needs 1)t_branch_info, 2)t_stop_locations
+python3 make_walk_dist_db.py #gives green_line_walking_distances #needs 1)t_branch_info, 2)t_stop_locations
 
 python3 make_nearest_stops.py #needs green_line_walking_distances
 
