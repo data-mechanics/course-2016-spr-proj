4e11c28939366d2a628ac02a2ff93f5beb5966e3
dianaantipode <chang.diana.gao@gmail.com>
2016-05-29 14:47:12 -0400

    running script, problem not fixed

--- jtsliu_kmann/run.sh
+++ jtsliu_kmann/run.sh
@@ -1,16 +1,16 @@
 #Data Retrieval
 #python3 fetch_liquor_data.py 
 #python3 fetch_property_assessment.py 
-python3 fetch_crime_data.py 
+#python3 fetch_crime_data.py    #main file;  HTTP Error 429: Too Many Requests
 #python3 fetch_public_school_data.py 
-#python3 fetch_hospital_data.py
+python3 fetch_hospital_data.py
 
-python3 create_zipcode_with_liquor_and_property_value.py
-python3 reverse_geocode.py
+#python3 create_zipcode_with_liquor_and_property_value.py
+#python3 reverse_geocode.py     #HTTP Error 429: Too Many Requests
 
-python3 create_crime_near_alcohol.py
-python3 create_zipcode_profile.py
+#python3 create_crime_near_alcohol.py   #name 'uuid' is not defined
+#python3 create_zipcode_profile.py
 
-python3 calculate_crime_liquor_correlation.py
-python solve_optimal_zipcode.py
-python3 query_zipcodes.py
+#python3 calculate_crime_liquor_correlation.py
+#python solve_optimal_zipcode.py      #No module named prov.model
+#python3 query_zipcodes.py
