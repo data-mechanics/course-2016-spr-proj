2cb4ebd1d87a654cbda308cf5e6e230c5aed9096
dianaantipode <chang.diana.gao@gmail.com>
2016-06-01 21:27:17 -0400

    running script

--- linshan_luoty/run.sh
+++ linshan_luoty/run.sh
@@ -1,8 +1,8 @@
 python3 retrieve_datasets.py ../auth.json
-python3 extract_zip_info.py ../auth.json
-python3 update_crime_lng_lat ../auth.json
-python3 merge_crime_with_zip ../auth.json
-python3 extract_earning_zips ../auth.json
-python3 get_avg_earning ../auth.json
-python3 get_total_crimes ../auth.json
-python3 merge_zip_crime_earnings ../auth.json
+#python3 extract_zip_info.py ../auth.json
+#python3 update_crime_lng_lat ../auth.json
+#python3 merge_crime_with_zip ../auth.json
+#python3 extract_earning_zips ../auth.json
+#python3 get_avg_earning ../auth.json
+#python3 get_total_crimes ../auth.json
+#python3 merge_zip_crime_earnings ../auth.json
