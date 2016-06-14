bb4c33723ac19beaf129335ac85313b2abe817b7
root <root@ip-172-31-16-87.ec2.internal>
2016-06-14 03:04:16 +0000

    Fix issue causing error if plan.json file does not yet exist.

--- linshan_luoty/provenance.py
+++ linshan_luoty/provenance.py
@@ -1,10 +1,14 @@
 import prov
+import prov.model
 import json
+import os.path
 
 def init():
-	f = open('plan.json', 'r')
-	doc = prov.read(f)
-	f.close()
+	doc = prov.model.ProvDocument()
+	if os.path.isfile('plan.json'):
+		f = open('plan.json', 'r')
+		doc = prov.read(f)
+		f.close()
 	doc.add_namespace('alg', 'https://data-mechanics.s3.amazonaws.com/linshan_luoty/algorithm/') # The scripts in <folder>/<filename> format.
 	doc.add_namespace('dat', 'https://data-mechanics.s3.amazonaws.com/linshan_luoty/data/') # The data sets in <user>/<collection> format.
 	doc.add_namespace('ont', 'https://data-mechanics.s3.amazonaws.com/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
