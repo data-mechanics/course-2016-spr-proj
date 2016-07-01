dd3ec6ea8b4ffbb3f414134adb7e73efd6fa9f97
Andrei Lapets <alapets@bbn.com>
2016-07-01 17:36:09 -0400

    Add "with" construct to example script.

--- alice_bob/retrieve.py
+++ alice_bob/retrieve.py
@@ -30,19 +30,21 @@ response = urllib.request.urlopen(url).read()
 z = zipfile.ZipFile(io.BytesIO(response))
 for file in z.namelist():
     if file in ['boston_massachusetts_osm_line.geojson', 'boston_massachusetts_osm_point.geojson']:
-        geojson = json.loads(z.open(file).read().decode("latin-1"))
-        repo['lapets.osmboston'].insert_many(geojson['features'])
-        # If we want to write the file to disk.
-        # open(name, 'wb').write(z.open(file).read())
+        with z.open(file) as f:
+            raw = f.read().decode("latin-1")
+            geojson = json.loads(raw)
+            repo['lapets.osmboston'].insert_many(geojson['features'])
+            # If we want to write the file to disk.
+            # open(name, 'wb').write(z.open(file).read())
 
-        # Write an example file to disk (for leaflet.js example)
-        # that contains a random sample of the features.
-        if file == 'boston_massachusetts_osm_line.geojson':
-            # Reload the data since .insert_many() backfills ObjectId fields,
-            # which can't be serialized with json.dumps().
-            geojson = json.loads(z.open(file).read().decode("latin-1"))
-            geojson['features'] = random.sample(geojson['features'], 50)
-            open('example.geojson', 'w').write("var example = " + json.dumps(geojson, sort_keys=True, indent=2) + ";")
+            # Write an example file to disk (for leaflet.js example)
+            # that contains a random sample of the features.
+            if file == 'boston_massachusetts_osm_line.geojson':
+                # Reload the data since .insert_many() backfills ObjectId fields,
+                # which can't be serialized with json.dumps().
+                geojson = json.loads(raw)
+                geojson['features'] = random.sample(geojson['features'], 50)
+                open('example.geojson', 'w').write("var example = " + json.dumps(geojson, sort_keys=True, indent=2) + ";")
 
 # Close the database connection.
 repo.logout()
